b6a25c3d5608dcc00ddc862e6d850573b62323d9
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2020-06-14 14:52:27 +0100

    Introduction to Testing with Enzyme

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { requestRobots, searchRobots } from './reducers'
 
+
 import './index.css';
 
 const logger = createLogger()
@@ -25,3 +26,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 registerServiceWorker();
+
+
